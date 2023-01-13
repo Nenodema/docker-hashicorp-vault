44b13b927d3e88eba6167250322514d5901a97af
Nenodema <33698556+Nenodema@users.noreply.github.com>
2023-01-13 15:43:14 +0100

    Create read_vault_v1_python.py

---
+++ examples/python/read_vault_v1_python.py
@@ -0,0 +1,17 @@
+import hvac
+
+client = hvac.Client(
+    url='http://VAULT_IP:8200/',
+    token='TOKEN',
+)
+
+auth_response = client.is_authenticated()
+
+read_response = client.secrets.kv.v1.read_secret(mount_point='demo_v1', path='server1')
+
+username = read_response['data']['username']
+password = read_response['data']['password']
+
+print('Authenticated:', auth_response)
+print('Username from vault:', username)
+print('Secret from vault:', password)
